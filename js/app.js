55b2b218584518cdf87c3e8341f6c0d73aa4cf18
shamima <shamimayesmin144@gamil.com>
2022-09-04 11:37:25 +0600

    new added

--- js/app.js
+++ js/app.js
@@ -82,7 +82,7 @@ const displayAllNews = news =>{
                 
                 
                 </div>
-                <h6>${author.published_date}</h6>
+                <h6 class="mr-20 mb-10">${author.published_date}</h6>
                 <div class="card-actions justify-end">
 
                 
